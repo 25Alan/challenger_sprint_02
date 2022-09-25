672098f4a0ebe3426b20549a809df8a71ae51f28
25Alan <romeroalan71@gmail.com>
2022-09-25 03:02:56 -0300

    Probando Local Storage

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let localStorage = window.localStorage;
+
+localStorage.getItem('uno','1');
+localStorage.setItem('dos','2');
+localStorage.setItem('tres','3');
+localStorage.setItem('cuatro','4');
+localStorage.setItem('cinco','5');
+localStorage.setItem('seis','6');
+
+console.log(localStorage.getItem('dos'));
