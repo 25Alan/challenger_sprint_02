581218b2cc765eae0dcd0d2edab97bee5d46314c
25Alan <romeroalan71@gmail.com>
2022-09-27 02:15:04 -0300

    Inicio de la lógica del juego

--- js/script.js
+++ js/script.js
@@ -1,10 +1,55 @@
-let localStorage = window.localStorage;
+const PALABRAS = ['alura','html','css','javascript','git','node'];
 
-localStorage.getItem('uno','1');
-localStorage.setItem('dos','2');
-localStorage.setItem('tres','3');
-localStorage.setItem('cuatro','4');
-localStorage.setItem('cinco','5');
-localStorage.setItem('seis','6');
+var maxIntento = PALABRAS[Math.floor(Math.random() * PALABRAS.length)];
 
-console.log(localStorage.getItem('dos'));
+
+/* CANVAS */
+var canvas = document.querySelector("canvas").getContext("2d");
+
+// canvas.fillStyle = 'white';
+// canvas.beginPath();
+// canvas.moveTo(40,50);
+// canvas.lineTo(60,50);
+// canvas.stroke();
+
+// canvas.fillStyle = 'blue';
+// canvas.beginPath();
+// canvas.moveTo(80,50);
+// canvas.lineTo(100,50);
+// canvas.stroke();
+
+// canvas.fillStyle = 'blue';
+// canvas.beginPath();
+// canvas.moveTo(120,50);
+// canvas.lineTo(140,50);
+// canvas.stroke();
+
+// canvas.fillStyle = 'blue';
+// canvas.beginPath();
+// canvas.moveTo(160,50);
+// canvas.lineTo(180,50);
+// canvas.stroke();
+
+// canvas.fillStyle = 'blue';
+// canvas.beginPath();
+// canvas.moveTo(200,50);
+// canvas.lineTo(220,50);
+// canvas.stroke();
+
+// canvas.fillStyle = 'blue';
+// canvas.beginPath();
+// canvas.moveTo(240,50);
+// canvas.lineTo(260,50);
+// canvas.stroke();
+
+// canvas.fillStyle = 'blue';
+// canvas.beginPath();
+// canvas.moveTo(280,50);
+// canvas.lineTo(300,50);
+// canvas.stroke();
+
+// canvas.fillStyle = 'blue';
+// canvas.beginPath();
+// canvas.moveTo(320,50);
+// canvas.lineTo(340,50);
+// canvas.stroke();
